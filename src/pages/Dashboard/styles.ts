dacdfd037544ebb78f761ed7e01acc25f60d863e
gyuu-neves <gyupro5@gmail.com>
2020-12-30 14:31:49 -0300

    added logo and styled some components.

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -3,4 +3,24 @@ import styled from 'styled-components';
 export const Title = styled.h1`
   font-size: 48px;
   color: #3a3a3a;
+  margin-top: 102px;
+  max-width: 450px;
+  line-height: 56px;
+`;
+
+export const Form = styled.form`
+  margin-top:40px;
+  max-width:714px;
+  display:flex;
+  max-height:72px;
+
+  input {
+    flex:1;
+  }
+
+  button {
+    width:210px;
+    height:72px;
+  }
+
 `;
