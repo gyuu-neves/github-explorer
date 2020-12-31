d2e235d934f12f8b78b4d5153fa90f3432d0380b
gyuu-neves <gyupro5@gmail.com>
2020-12-31 16:52:56 -0300

    styled div repo

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 
+import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => (
   <>
@@ -13,6 +14,19 @@ const Dashboard: React.FC = () => (
       <input placeholder="Digite o nome do repositório" />
       <button type="submit">Pesquisar</button>
     </Form>
+    <Repositories>
+      <a href="test">
+        <img
+          src="https://avatars2.githubusercontent.com/u/19414168?s=460&u=1a8bfaa8153d781fbc8c0432b1a95ec7e7e7eba0&v=4"
+          alt="Gyuu August"
+        />
+        <div>
+          <strong>gyuu-neves/next-learn-starter</strong>
+          <p>gyuu-neves/next-learn-starter</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
   </>
 );
 export default Dashboard;
